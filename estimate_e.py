52f3ad28d80080ae39279bb6d7d91291c5594faf
Israel Flores <iflores10@miners.utep.edu>
2017-03-13 03:52:59 -0600

    redefined from hits to misses

--- estimate_e.py
+++ estimate_e.py
@@ -1,69 +1,70 @@
 """
 @author: Israel Flores
-
 This program estimates the number e with a random binary matrix. This program is free software. 
 """
-
 import numpy as np
 import matplotlib.pyplot as plt   
 
 class RandomBinaryMatrix(object):
     
     def __init__(self, n):           
-        self.n = n
-        self.length = 2**(n+1)#side length of square matrix   
+        self.n = n# n also automatically defines the cluster size
+        self.length = 2**(n+1)#side length of the square matrix  
+        #randomly build the matrix we intend to search for non-clustered rows and columns:
         self.matrix = np.random.randint(2, size=(self.length, self.length))
-        self.totalTrials = 2**(n+3)# (rows + columns)*2     
+        self.totalTrials = 2**(n+3)#  = 2*(rows+columns)    
         
     def plotResult(self):
         #declare x-axis for graph
         self.trialsVector = np.linspace(1, self.totalTrials, self.totalTrials) 
-        #plot the real value of e as a horizontal line                     
-        plt.plot([0, self.totalTrials],[np.e, np.e],'m',ls='dashed',label=('Real value of e = 2.71828...')) 
-        #plot trials/(trials - hits)
-        plt.plot(self.trialsVector, self.getResultsVector(), label='Estimation of e')        
+        #plot the real value of e as a dashed horizontal line                     
+        plt.plot([0, self.totalTrials],[np.e, np.e],'m',ls='dashed',label=('Real value of e = 2.71828...'))         
+        plt.plot(self.trialsVector, self.getRatioVector(), label='Estimation of e')#plots trials/clusterMisses     
         plt.title(self.getTitleForGraph())
         plt.xlabel("Trials\n(i.e. rows and columns observed)")
-        plt.ylabel("Trials/(Trials - Hits)")
+        plt.ylabel("Trials/ClusterMisses")
         plt.legend()
         plt.show()
         print("Final result is e ~ " + self.getFinalEstimation(8) + " (see graph)\n")
         
-    #this method returns the ratio trials/(trials - hits) at each step (i.e. at each trial number)         
-    def getResultsVector(self):
-        self.hitsVector = np.empty(self.totalTrials)#keeps track of the "hits" in vector format
+    #i.e. returns the ratio trials/clusterMisses at every stage of the search     
+    def getRatioVector(self):
+        #declare an array that stores the cluster-miss count with the trial number  as its index
+        self.missesVector = np.empty(self.totalTrials)
         self.trialsCount = 0
-        self.hitsCount = 0 
-        self.searchMatrix(1)#find clusters of ones
-        self.searchMatrix(0)#find clusters of zeros        
-        return self.trialsVector/(self.trialsVector - self.hitsVector)        
+        self.clusterMisses = 0 #counter for both 1-cluster-misses and 0-cluster-misses
+        self.searchMatrix(1)#find 1-cluter-misses in rows and columns
+        self.searchMatrix(0)#find 0-cluter-misses in rows and columns
+        return self.trialsVector/self.missesVector       
         
-    def searchMatrix(self, binaryDigit):#binaryDigit must be a one or a zero
-        #search rows    
+    def searchMatrix(self, binaryDigit):#binaryDigit must be a one or a zero (the cluster-type)
+        #search all of the rows    
         for row in range(0, self.length):
-            clusterSize = 0
+            self.clusterMisses += 1#assume the row we're about to observe has no cluster
+            streak = 0 #keeps track of the current number of consecutive binaryDigits 
             for nextDigit in self.matrix[row, 0:self.length]:
                 if nextDigit == binaryDigit:
-                    clusterSize += 1
-                    if clusterSize >= self.n:#true when a cluster is found
-                        self.hitsCount += 1
+                    streak += 1
+                    if streak >= self.n:#true when a cluster is found
+                        self.clusterMisses -= 1#make up for assuming it had no cluster
                         break#no need to continue checking the same row when a cluster is found
-                else: clusterSize = 0 
+                else: streak = 0 
             #update data:
-            self.hitsVector[self.trialsCount] = self.hitsCount
+            self.missesVector[self.trialsCount] = self.clusterMisses
             self.trialsCount += 1   
-        #search columns        
+        #search all of the columns        
         for column in range(0, self.length):
-            clusterSize = 0
-            for nextDigit in self.matrix[0:self.length, column]:
+            self.clusterMisses += 1#assume the colunm we're about to observe has no cluster
+            streak = 0 #keeps track of the current number of consecutive binaryDigits 
+            for nextDigit in self.matrix[0:self.length, column]:                
                 if nextDigit == binaryDigit:
-                    clusterSize += 1
-                    if clusterSize >= self.n:#true when a cluster is found
-                        self.hitsCount += 1
+                    streak += 1
+                    if streak >= self.n:#true when a cluster is found
+                        self.clusterMisses -= 1#make up for assuming it had no cluster
                         break#no need to continue checking the same column when a cluster is found
-                else: clusterSize = 0 
+                else: streak = 0 
             #update data:
-            self.hitsVector[self.trialsCount] = self.hitsCount
+            self.missesVector[self.trialsCount] = self.clusterMisses
             self.trialsCount += 1       
         
     def getTitleForGraph(self):
@@ -73,7 +74,7 @@ class RandomBinaryMatrix(object):
         return firstTitleLine + secondTitleLine
     
     def getFinalEstimation(self, decimals=5):
-        return str(format(self.totalTrials/(self.totalTrials - self.hitsCount), '.' + str(decimals) + 'f'))
+        return str(format(self.totalTrials/self.clusterMisses, '.' + str(decimals) + 'f'))
 # instantiate and plot with n = 10:       
 m = RandomBinaryMatrix(10)
 m.plotResult() 
